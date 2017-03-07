8069b573f1b8e0dafd8c2c6eaceb76d497e0688a
xtzeng <xiaoti_zeng@126.com>
2017-03-07 15:30:10 +0800

    add spring-boot-test.sql

---
+++ xmq-spring-boot-mybatis-xml/docs/spring-boot-test.sql
@@ -0,0 +1,24 @@
+﻿# Host: 120.25.72.178  (Version 5.6.26-log)
+# Date: 2017-03-06 16:56:19
+# Generator: MySQL-Front 6.0  (Build 1.44)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "user"
+#
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `user_name` varchar(32) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(32) DEFAULT NULL COMMENT '密码',
+  `user_sex` varchar(32) DEFAULT NULL,
+  `nick_name` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "user"
+#
+
